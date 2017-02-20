dca44597f2a0b7bcd5128924ee2642fca377240c
Thomas Mays <MindOfThomas@users.noreply.github.com>
2017-02-20 15:08:12 -0500

    Add game settings

---
+++ src/js/App.js
@@ -0,0 +1,11 @@
+let settings = {
+  boardSize: [50, 25],
+  rules: {
+    numsToSpawn: [3],
+    numsToSurvive: [2, 3],
+    numsToDie: [0, 1, 4, 5, 6, 7, 8]
+  },
+  paused: true,
+  speed: 200,
+  speedSteps: [500, 200, 50]
+};

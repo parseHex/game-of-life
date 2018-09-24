8cb88471c5ed7d69078e77e02f667afac6f60d63
Thomas <1779937+parseHex@users.noreply.github.com>
2018-09-23 23:23:38 -0400

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,15 @@
 const path = require('path');
-// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
-// const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 module.exports = {
 	mode: 'development',
 	devtool: 'cheap-source-map',
 	// devtool: 'cheap-module-eval-source-map',
 	entry: {
-		main: './index.js',
+		main: './js/App.js',
 	},
 	output: {
-		path: path.join(__dirname, '../build/js'),
-		filename: `[name].bundle.js`,
+		path: path.join(__dirname, './build/js'),
+		filename: `bundle.js`,
 	},
 	resolve: {
 		// extensions: ['.ts'],
@@ -21,20 +18,8 @@ module.exports = {
 		// rules: [{
 		// 	test: /\.ts$/,
 		// 	use: [{
-		// 		loader: 'ts-loader',
-		// 		options: {
-		// 			transpileOnly: true,
-		// 			experimentalWatchApi: true,
-		// 		},
+		// 		loader: 'awesome-typescript-loader',
 		// 	}],
 		// }],
 	},
-	optimization: {
-		// removeAvailableModules: false,
-		// removeEmptyChunks: false,
-		// splitChunks: false,
-	},
-	plugins: [
-		// new ForkTsCheckerWebpackPlugin(),
-	],
 };
